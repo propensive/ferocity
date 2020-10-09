cf7a0db9b54e19943f6a25741d7df6952d54cd65
Tomasz Dudzik <tomaszdudzik.web@gmail.com>
2020-10-09 19:49:41 +0200

    Sort items (projects, modules, etc.) alphabetically

--- src/fury.ts
+++ src/fury.ts
@@ -45,28 +45,40 @@ export namespace layer {
           return SourceType.Unknown;
       }
     };
-    const getDependencies = (module: any) => module.dependencies.map((dependency: any) => dependency.ref.id);
+    const getDependencies = (module: any) => module.dependencies
+      .map((dependency: any) => dependency.ref.id)
+      .sort((a: string, b: string) => a < b ? -1 : 1);
     // TODO: handle other types of sources
-    const getSources = (module: any) => module.sources.map((source: any) => ({
-      'directory': source.dir.input,
-      'type': getSourceType(source._type)
-    }));
-    const getBinaries = (module: any) => module.binaries.map((binary: any) => binary.id.key);
-    const getModules = (project: any) => project.modules.map((module: any) => ({
-      name: module.id.key,
-      dependencies: getDependencies(module),
-      sources: getSources(module),
-      binaries: getBinaries(module)
-    }));
-    const getProjects = (layer: any) => layer.projects.map((project: any) => ({
-      name: project.id.key,
-      modules: getModules(project)
-    }));
+    const getSources = (module: any) => module.sources
+      .map((source: any) => ({
+        'directory': source.dir.input,
+        'type': getSourceType(source._type)
+      }))
+      .sort((a: Source, b: Source) => a.directory < b.directory ? -1 : 1);
+    const getBinaries = (module: any) => module.binaries
+      .map((binary: any) => binary.id.key)
+      .sort((a: string, b: string) => a < b ? -1 : 1);
+    const getModules = (project: any) => project.modules
+      .map((module: any) => ({
+        name: module.id.key,
+        dependencies: getDependencies(module),
+        sources: getSources(module),
+        binaries: getBinaries(module)
+      }))
+      .sort((a: Module, b: Module) => a.name < b.name ? -1 : 1);
+    const getProjects = (layer: any) => layer.projects
+      .map((project: any) => ({
+        name: project.id.key,
+        modules: getModules(project)
+      }))
+      .sort((a: Project, b: Project) => a.name < b.name ? -1 : 1);
     console.log(`Get layer from workspace: ${workspace}`);
     const furyServerUrl = 'http://localhost:6325/layer?path=' + workspace;
-    return axios.get(furyServerUrl).then(response => ({
-      name: '/',
-      projects: getProjects(response.data)
-    }));
+    return axios
+      .get(furyServerUrl)
+      .then(response => ({
+        name: '/',
+        projects: getProjects(response.data)
+      }));
   }
 }
