ead60477d7c54356a0d20a7893dd71a5e9a55bf0
Tomasz Dudzik <tomaszdudzik.web@gmail.com>
2020-11-20 17:20:28 +0100

    Add Scala icon for scala files in the layer tree

--- src/tree/layerTree.ts
+++ src/tree/layerTree.ts
@@ -106,6 +106,7 @@ class SourceFileItem extends FerocityTreeItem {
 			command: 'ferocity.openFile',
 			arguments: [path, isLocal]
 		};
+		this.iconPath = path.endsWith('.scala') ? createIconPath('scala') : undefined;
 	}
 }
 
